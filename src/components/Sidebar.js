38fff76260e6aeafef664cea2af01bfa7ab1c430
Shubham_Jaiswal <shubhamj@advantageclub.co>
2024-02-28 13:36:52 +0530

    updated close all except itself

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,18 +3,23 @@ import header from "../data/header.json";
 import { useState } from "react";
 
 export default function Sidebar() {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const handleItemClick = (index, hasContent) => {
+    setOpenIndex(hasContent ? (index === openIndex ? null : index) : null);
+  };
 
   return (
     <div className="sidebar">
-      {header
-        .sort((a, b) => b.priority - a.priority)
-        ?.map((item, index) => (
-          <SidebarItem
-            key={index}
-            item={item}
-            activeLink={""}
-          />
-        ))}
+      {header.sort((a, b) => b.priority - a.priority).map((item, index) => (
+        <SidebarItem
+          key={index}
+          item={item}
+          activeLink=""
+          open={openIndex === index}
+          setOpen={(hasContent) => handleItemClick(index, hasContent)}
+        />
+      ))}
     </div>
   );
 }
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -1,39 +1,50 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import modules from "../data/modules.json";
 import { Link, useLocation } from "react-router-dom";
 
-export default function SidebarItem({ item, activeLink }) {
-
-  const { pathname, search } = useLocation();
-  const [open, setOpen] = useState(modules[item?.key]?.some((ele) => ele?.link == pathname));
-
-  return !!modules[item?.key] ? (
-    <div className={"sidebar-item"}>
-      <div className="sidebar-title cursor-pointer flex items-center" onClick={()=>  setOpen(!open)}>
-        <span className="flex justify-center items-center gap-x-4"> 
-          {item.icon && <img src={item.icon} className="w-7"></img>}
-         <p>{item.field_name}</p> 
-        </span>
-        <i className={`bi-chevron-down ${open && "toggle-btn"}`}></i>
-      </div>
-      { open && (
-        <div className="sidebar-content">
-          {modules[item?.key].map((child, index) => (
-            <SidebarItem key={child.field_name + index} item={child} activeLink={activeLink} />
-          ))}
+export default function SidebarItem({ item, activeLink, open, setOpen }) {
+    const { pathname, search } = useLocation();
+  
+    const handleClick = () => {
+      setOpen(!open);
+    };
+  
+    return !!modules[item?.key] ? (
+      <div className="sidebar-item">
+        <div
+          className="sidebar-title cursor-pointer flex items-center"
+          onClick={handleClick}
+        >
+          <span className="flex justify-center items-center gap-x-4">
+            {item.icon && <img src={item.icon} className="w-7" alt="Icon" />}
+            <p>{item.field_name}</p>
+          </span>
+          <i className={`bi-chevron-down ${open && "toggle-btn"}`}></i>
         </div>
-      )}
-    </div>
-  ) : (
-    
-    <Link
-      to={item?.link}
-      className={`sidebar-item plain ${pathname + search === item?.link ? "active" : ""} flex gap-x-4 items-center`}
-    >
-      {item.icon && <img src={item.icon} className="w-7"></img>}
-     <p> {item.field_name}</p>
-    </Link>
-  );
-}
-
-
+        {open && (
+          <div className="sidebar-content">
+            {modules[item?.key].map((child, index) => (
+              <SidebarItem
+                key={child.field_name + index}
+                item={child}
+                activeLink={activeLink}
+                open={open}
+                setOpen={setOpen}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    ) : (
+      <Link
+        to={item?.link}
+        className={`sidebar-item plain ${
+          pathname + search === item?.link ? "active" : ""
+        } flex gap-x-4 items-center`}
+        onClick={() => setOpen(false)} // Close all sidebar titles when clicked
+      >
+        {item.icon && <img src={item.icon} className="w-7" alt="Icon" />}
+        <p>{item.field_name}</p>
+      </Link>
+    );
+  }
